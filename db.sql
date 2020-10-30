72f56436c6f7f20fd5a5dfb3f27855729ed3276b
yuta-komura <you@example.com>
2020-10-31 00:52:52 +0900

    add db設定ファイル

---
+++ db.sql
@@ -0,0 +1,21 @@
+-- データベース
+CREATE DATABASE tradingbot;
+
+-- テーブル
+CREATE TABLE `entry` (`side` varchar(255) NOT NULL);
+
+CREATE TABLE `execution_history` (
+    `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+    `date` datetime(6) NOT NULL,
+    `side` varchar(255) NOT NULL,
+    `price` int unsigned NOT NULL,
+    `size` decimal(65, 30) unsigned NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `index_execution_history_1` (`date`)
+);
+
+CREATE TABLE `ticker` (
+    `date` timestamp NOT NULL,
+    `best_bid` int unsigned NOT NULL,
+    `best_ask` int unsigned NOT NULL
+);
